b3857344f90b8d3ca679acd4477056d4f9d62d01
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-09-15 11:29:28 +0300

    +Task3

--- SystemCollection/Task3/Program.cs
+++ SystemCollection/Task3/Program.cs
@@ -13,6 +13,25 @@ namespace Task3
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            Dictionary<int, double> corp = new Dictionary<int, double>();
+            corp.Add(random.Next(1000,9999),random.NextDouble()*100);
+            corp.Add(random.Next(1000, 9999), random.NextDouble()*100);
+            corp.Add(random.Next(1000, 9999), random.NextDouble()*100);
+            foreach (var item in corp)
+            {
+                Console.WriteLine("{0} {1} млн$",item.Key,item.Value);
+            }
+            Console.WriteLine(new string('_',20));
+            SortedDictionary<int, double> corp2 = new SortedDictionary<int, double>();
+            corp2.Add(random.Next(1000, 9999), random.NextDouble() * 100);
+            corp2.Add(random.Next(1000, 9999), random.NextDouble() * 100);
+            corp2.Add(random.Next(1000, 9999), random.NextDouble() * 100);
+            foreach (var item in corp2)
+            {
+                Console.WriteLine("{0} {1} млн$", item.Key, item.Value);
+            }
+            Console.ReadKey();
         }
     }
 }
