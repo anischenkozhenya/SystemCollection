fd6182ee69d51666c1127fe44944bcd59b9f6890
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-09-15 13:30:48 +0300

    +Task4

--- SystemCollection/Task4/Program.cs
+++ SystemCollection/Task4/Program.cs
@@ -10,15 +10,37 @@ using System.Threading.Tasks;
 //ключей.
 namespace Task4
 {
-    static class Program
-    {  static public bool Compare(this OrderedDictionary orderedDictionary,int x,int y)
+    class MyClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    class MyClassComparer : IEqualityComparer
+    {
+        public new bool Equals(object x, object y)
         {
-            if (orderedDictionary[x]==orderedDictionary[y])
+            if ((x as MyClass).Id==(y as MyClass).Id)
             {
                 return true;
             }
             return false;
         }
+
+        public int GetHashCode(object obj)
+        {
+            return (obj as MyClass).Id;
+        }
+    }
+    static class Program
+    {        
+        //static public bool Compare(this OrderedDictionary orderedDictionary,int x,int y)
+        //{
+        //    if (orderedDictionary[x]==orderedDictionary[y])
+        //    {
+        //        return true;
+        //    }
+        //    return false;
+        //}
         static void Main(string[] args)
         {
             
@@ -34,7 +56,31 @@ namespace Task4
                 Console.WriteLine("{0} {1}",item.Key,item.Value);
             }
             Console.WriteLine(new string('-',20));
-            Console.WriteLine(orderedDictionary.Compare(2,5));
+            //Console.WriteLine(orderedDictionary.Compare(2,5));
+            OrderedDictionary orderedDictionary1 = new OrderedDictionary(new MyClassComparer());
+            MyClass myClass1 = new MyClass() { Id = 1, Name = "Женя" };
+            MyClass myClass2 = new MyClass() { Id = 2, Name = "Вася" };
+            MyClass myClass3 = new MyClass() { Id = 3, Name = "Денис" };
+            MyClass myClass4 = new MyClass() { Id = 4, Name = "Дима" };
+            MyClass myClass5 = new MyClass() { Id = 4, Name = "Андрей"};
+            try
+            {
+                orderedDictionary1.Add(myClass1, "Student");
+                orderedDictionary1.Add(myClass2, "Worker");
+                orderedDictionary1.Add(myClass3, "Pensioner");
+                orderedDictionary1.Add(myClass4, "Jobless");
+                orderedDictionary1.Add(myClass5, "Pupil");//Ошибка: Совпадение ID
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Такой ключ уже есть!");
+                
+            }
+            foreach (DictionaryEntry item in orderedDictionary1)
+            {
+                Console.WriteLine("{0} {1} {2}", ((MyClass)item.Key).Id, ((MyClass)item.Key).Name, item.Value);
+            }
+
             Console.ReadKey();
         }
     }
